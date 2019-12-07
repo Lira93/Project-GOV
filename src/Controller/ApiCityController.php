61111f59afdebf11b174ddebca6fb6e92cc7dc7b
Krystian Liris <krystianiko@gmail.com>
2019-12-07 19:33:41 +0100

    List stations&cities

---
+++ src/Controller/ApiCityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ApiCityController extends AbstractController
+{
+    /**
+     * @Route("/api/city", name="api_city")
+     */
+    public function index(HttpClientInterface $httpClient)
+    {
+        $response = $httpClient->request('GET','http://api.gios.gov.pl/pjp-api/rest/station/findAll');
+
+        $var1 = $response->toArray();
+        //$json = $var->toArray();
+
+
+        return $this->render('api_city/index.html.twig', [
+            'controller_name' => 'ApiCityController',
+            'cities' => $response->toArray(),
+        ]);
+    }
+    /**
+     * @Route("/api/city/show/{id}", name="show")
+     */
+    public function show($id, HttpClientInterface $httpClient)
+    {
+        $response = $httpClient->request('GET','http://api.gios.gov.pl/pjp-api/rest/station/sensors/'.$id);
+
+        $var = $response->toArray();
+
+
+        return $this->render('api_city/show.html.twig', [
+            'controller_name' => 'ApiCityController',
+            'cities' => $response->toArray(),
+        ]);
+
+    }
+}
